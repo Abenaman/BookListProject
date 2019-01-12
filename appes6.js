19158d9c4eeb9922c5834d4d4d65e5f597700294
Abenezer Berhanu <abenagood5@gmail.com>
2019-01-12 15:27:56 +0200

    Lastcommit

--- appes6.js
+++ appes6.js
@@ -9,9 +9,7 @@ class Book {
 class UI {
   addBookToList(book) {
     const list = document.getElementById('book-list');
-    // Create tr element
     const row = document.createElement('tr');
-    // Insert cols
     row.innerHTML = `
       <td>${book.title}</td>
       <td>${book.author}</td>
@@ -23,20 +21,12 @@ class UI {
   }
 
   showAlert(message, className) {
-    // Create div
     const div = document.createElement('div');
-    // Add classes
     div.className = `alert ${className}`;
-    // Add text
     div.appendChild(document.createTextNode(message));
-    // Get parent
     const container = document.querySelector('.container');
-    // Get form
     const form = document.querySelector('#book-form');
-    // Insert alert
     container.insertBefore(div, form);
-
-    // Timeout after 3 sec
     setTimeout(function(){
       document.querySelector('.alert').remove();
     }, 3000);
@@ -54,8 +44,11 @@ class UI {
     document.getElementById('isbn').value = '';
   }
 }
-
-// Local Storage Class
+/**
+**
+**@local storage 
+**
+*/
 class Store {
   static getBooks() {
     let books;
@@ -74,7 +67,6 @@ class Store {
     books.forEach(function(book){
       const ui  = new UI;
 
-      // Add book to UI
       ui.addBookToList(book);
     });
   }
@@ -100,58 +92,41 @@ class Store {
   }
 }
 
-// DOM Load Event
+/**
+**
+**@Event listner
+**
+*/
 document.addEventListener('DOMContentLoaded', Store.displayBooks);
 
-// Event Listener for add book
 document.getElementById('book-form').addEventListener('submit', function(e){
-  // Get form values
   const title = document.getElementById('title').value,
         author = document.getElementById('author').value,
         isbn = document.getElementById('isbn').value
-
-  // Instantiate book
   const book = new Book(title, author, isbn);
-
-  // Instantiate UI
   const ui = new UI();
 
   console.log(ui);
-
-  // Validate
+/**
+**
+**@returns alert message
+**
+*/
   if(title === '' || author === '' || isbn === '') {
-    // Error alert
     ui.showAlert('Please fill in all fields', 'error');
   } else {
-    // Add book to list
     ui.addBookToList(book);
-
-    // Add to LS
     Store.addBook(book);
-
-    // Show success
     ui.showAlert('Book Added!', 'success');
-  
-    // Clear fields
     ui.clearFields();
   }
 
   e.preventDefault();
 });
-
-// Event Listener for delete
 document.getElementById('book-list').addEventListener('click', function(e){
-
-  // Instantiate UI
   const ui = new UI();
-
-  // Delete book
   ui.deleteBook(e.target);
-
-  // Remove from LS
   Store.removeBook(e.target.parentElement.previousElementSibling.textContent);
-
-  // Show message
   ui.showAlert('Book Removed!', 'success');
 
   e.preventDefault();
